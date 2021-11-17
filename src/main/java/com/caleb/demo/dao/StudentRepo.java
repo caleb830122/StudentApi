93457683275e1ad1be4a0afe5b243bbbbb43a092
Caleb Chang <caleb830122@gmail.com>
2021-11-17 12:46:56 -0800

    Fix the REST Api links and create a React App project named "frontend"

--- src/main/java/com/caleb/demo/dao/StudentRepo.java
+++ src/main/java/com/caleb/demo/dao/StudentRepo.java
@@ -1,11 +1,10 @@
 package com.caleb.demo.dao;
 
-import org.springframework.data.repository.CrudRepository;
-
-import com.caleb.demo.model.Student;
-
-// CRUD operation implementation
-public interface StudentRepo extends CrudRepository<Student, Integer>{
-	// Includes all the generic CRUD method we need to access the database
-	// See CrudRepository for detail
+/**
+ * for CRUD operation
+ * @author calebchang
+ *
+ */
+public interface StudentRepo {
+	
 }
