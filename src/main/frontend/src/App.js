bab89d321d93e12de5ff4cdfd7f2e529430e2a54
Caleb Chang <caleb830122@gmail.com>
2021-11-22 21:45:09 -0800

    Organize student list component into studentList.js file

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,76 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import axios from 'axios';
-import {Table} from 'react-bootstrap';
-import StudentForm from './components/students';
+import StudentForm from './components/studentForm';
+import StudentList from './components/studentList';
 import NavBar from './components/navbar';
-
-const Students = () => {
-
-  // Similar to create a state object
-  // userProfile is the state being used, and we have a method called setUserProfiles to manipulate it
-  const [studentProfiles, setStudentProfiles] = useState([]);
-
-  const fetchAllStudent = () => {
-    axios.get("http://localhost:8080/student/getAllStudents").then(res => {
-      console.log(res);
-      // set userProfile state object with response data
-      setStudentProfiles(res.data)
-    });
-  };
-
-  useEffect(() => {
-    fetchAllStudent();
-  }, []);
-
-  return studentProfiles.map((studentProfile, index) => {
-    
-    return (
-      <tr key={index}>
-          <td>{studentProfile.id}</td>
-          <td>{studentProfile.firstName}</td>
-          <td>{studentProfile.lastName}</td>
-          <td>{studentProfile.major}</td>
-      </tr>
-    )
-  });
-};
-
-
-
-
-class StudentsList extends React.Component {
-  render() {
-    return (
-      <div className="student-list">
-        <div>
-          <h2>Student List</h2>
-        </div>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Major</th>
-            </tr>
-          </thead>
-          <tbody>
-            <Students />
-          </tbody> 
-        </Table>
-      </div>
-    );
-  }
-}
-
-
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <StudentsList />
+      <StudentList />
       <StudentForm />
     </div>
   );
--- src/main/frontend/src/components/navbar.js
+++ src/main/frontend/src/components/navbar.js
@@ -1,31 +1,54 @@
 import React from "react";
-import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
-
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 class NavBar extends React.Component {
-    render() {
-      return (
+  render() {
+    return (
+      <Router>
+        <div>
         <Navbar bg="myPurple" expand="lg" variant="dark" fixed="true">
-        <Container>
-          <Navbar.Brand href="#home">Student Information System Portal</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home">Search</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-        
-      );
-    }
+          <Container>
+            <Navbar.Brand as={Link} to={"/"}>
+              Student Information System Portal
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link as={Link} to={"/search"}>Search</Nav.Link>
+                <Nav.Link as={Link} to={"/link"}>Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        </div>
+        <div>
+          <Routes>
+            <Route path="/home" elements={<h1>Student</h1>} />
+            <Route path="/search" element={<h1>Search</h1>} />
+            
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default NavBar;
