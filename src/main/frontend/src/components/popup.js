80fab29ee80a4c8113dd09710fd7077dc7e33524
Caleb Chang <caleb830122@gmail.com>
2021-11-29 11:11:28 -0800

    Redesign popup, use it as a detail card of a student

--- src/main/frontend/src/components/popup.js
+++ src/main/frontend/src/components/popup.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Image } from 'react-bootstrap'
 import './popup.css'
 import StudentForm from './studentForm'
+import profileIcon from '../images/profile.png'
 
 function Popup(props) {
     return (props.trigger) ? (
         <div className="popup">
             <div className="popup-inner">
-                <h2 className="popup-title">Update {props.firstName} {props.lastName}</h2>
-                <StudentForm />
+                <div>  
+                    <Image src={profileIcon} width="20%"/>
+                </div>    
+                <h2 className="popup-title">{props.firstName} {props.lastName}</h2>
+                <h3>Major: {props.major}</h3>
                 <Button className="close-btn" onClick={() => props.setTrigger(false)}>close</Button>
                 { props.childern }
             </div>
