cccd045f03f4f68cf07c7d61052f1edd0a51c52d
Caleb Chang <caleb830122@gmail.com>
2021-12-06 15:14:51 -0800

    inMemoryAuth set up with spring starter security dependency

---
+++ src/main/java/com/caleb/demo/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.caleb.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication()
+		
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication()
+		.withUser("test")
+		.password(encoder.encode("test"))
+		.roles("USER");
+//		.withUser("test")
+//		.password("test")
+//		.roles("Admin")
+//		.and()
+//		.withUser("xyz")
+//		.password(" abc")
+//		.roles("Manager");
+	}
+}
