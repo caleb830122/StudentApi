537a6f98f702ac5fe788266205fa42b328241404
Caleb Chang <caleb830122@gmail.com>
2021-12-20 00:12:58 -0800

    Create user database

--- src/main/java/com/caleb/demo/security/SecurityConfig.java
+++ src/main/java/com/caleb/demo/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.caleb.demo.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -29,5 +30,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //		.withUser("xyz")
 //		.password(" abc")
 //		.roles("Manager");
+		
+		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		 http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+
 	}
 }
